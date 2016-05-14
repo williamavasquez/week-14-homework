54fd312c6d03845c6a940f2222002d7b16b722ea
williamavasquez <william.a.vasquez@gmail.com>
2016-05-13 21:53:11 -0400

    connection error

--- config/connection.js
+++ config/connection.js
@@ -13,12 +13,12 @@ if(process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect(function(err) {
+connection.connect(function(err){
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
-    }
+    } else {
     console.log('connected as id ' + connection.threadId);
 };
-
+});
 module.exports = connection;
